7d961989578242484fcda2a47f24db9cbc69308c
Imamasharmeen <imamasharmeen@gmail.com>
2024-09-24 02:38:32 +0300

    Create common function

--- js/script.js
+++ js/script.js
@@ -1,87 +1,91 @@
-//noakhali flooded
+
+//----------------noakhali flooded------------
+
 document.getElementById('noakhali-donation').addEventListener('click', function(){
     const noakhaliDonation = getInputFieldValueById('noakhali-donation-amount')
     const remainingBalance = getTextFieldValueById('remaining-balance')
     
 
-    if(noakhaliDonation < 0 || isNaN(noakhaliDonation)){
-        alert('Invalid Donation Amount')                           
+    if(noakhaliDonation <= 0 || noakhaliDonation >5500|| isNaN(noakhaliDonation)){
+        alert('Invalid Donation Amount') 
+        return;                          
     }
-    else{
-        const noakhaliTotalAmount = getTextFieldValueById('noakhali-total-amount')
-        const newNoakhaliBalance = noakhaliTotalAmount +noakhaliDonation;
-        document.getElementById('noakhali-total-amount').innerText = newNoakhaliBalance ;
-        
-    }
+    
+    const noakhaliTotalAmount = getTextFieldValueById('noakhali-total-amount')
+    const newNoakhaliBalance = noakhaliTotalAmount + noakhaliDonation;
+    document.getElementById('noakhali-total-amount').innerText = newNoakhaliBalance ;
 
     const newRemainingBalance = remainingBalance - noakhaliDonation;
-    document.getElementById('remaining-balance').innerText = newRemainingBalance; 
+    document.getElementById('remaining-balance').innerText = newRemainingBalance;   
+    
+    addHistory('Flood Relief in Noakhali', noakhaliDonation);
 
-    document.getElementById('my_modal_4').showModal();
-
-})
-
- // Add event listener for show the modal 
-
- document.getElementById('noakhali-donation').addEventListener('click', function(){
     document.getElementById('my_modal_4').showModal();
 })
 
-//Feni flooded
+//----------------------Feni flooded----------------------
 document.getElementById('feni-donation').addEventListener('click', function(){
     const feniDonation = getInputFieldValueById('feni-donation-amount')
     const remainingBalance = getTextFieldValueById('remaining-balance')
     
 
-    if(feniDonation < 0 || isNaN(feniDonation)){
-        alert('Invalid Donation Amount')                           
+    if(feniDonation <= 0 || feniDonation > 5500 || isNaN(feniDonation)){
+        alert('Invalid Donation Amount') 
+        return;                          
     }
-    else{
-        const feniTotalAmount = getTextFieldValueById('feni-total-amount')
-        const newFeniBalance = feniTotalAmount + feniDonation;
-        document.getElementById('feni-total-amount').innerText = newFeniBalance ;
-        
-    }
+
+    const feniTotalAmount = getTextFieldValueById('feni-total-amount')
+    const newFeniBalance = feniTotalAmount + feniDonation;
+    document.getElementById('feni-total-amount').innerText = newFeniBalance ;
 
     const newRemainingBalance = remainingBalance - feniDonation;
     document.getElementById('remaining-balance').innerText = newRemainingBalance;
-    
-   
-})
- // Add event listener for show the modal
 
-document.getElementById('feni-donation').addEventListener('click', function(){
+    addHistory('Flood Relief in Feni', feniDonation);
+
     document.getElementById('my_modal_4').showModal();
+      
 })
 
-//Quota movement
+//---------------Quota movement------------------
+
 document.getElementById('quota-donation').addEventListener('click', function(){
     const quotaDonation = getInputFieldValueById('quota-donation-amount')
     const remainingBalance = getTextFieldValueById('remaining-balance')
     
 
-    if(quotaDonation < 0 || isNaN(quotaDonation)){
-        alert('Invalid Donation Amount')                           
-    }
-    else{
-        const quotaTotalAmount = getTextFieldValueById('quota-total-amount')
-        const newQuotaBalance = quotaTotalAmount + quotaDonation;
-        document.getElementById('quota-total-amount').innerText = newQuotaBalance ;
-        
+    if(quotaDonation <=0 || quotaDonation > 5500|| isNaN(quotaDonation)){
+        alert('Invalid Donation Amount') 
+        return;                          
     }
 
+    const quotaTotalAmount = getTextFieldValueById('quota-total-amount')
+    const newQuotaBalance = quotaTotalAmount + quotaDonation;
+    document.getElementById('quota-total-amount').innerText = newQuotaBalance ;
+    
     const newRemainingBalance = remainingBalance - quotaDonation;
-    document.getElementById('remaining-balance').innerText = newRemainingBalance;
+     document.getElementById('remaining-balance').innerText = newRemainingBalance;
+
+    addHistory('Aid for Injured in the Quota Movement', quotaDonation);
+
+    document.getElementById('my_modal_4').showModal();
     
 })
 
- // Add event listener for show the modal
+//-------------history section--------------------
 
- document.getElementById('quota-donation').addEventListener('click', function(){
-    document.getElementById('my_modal_4').showModal();
+document.getElementById('history').addEventListener('click', function(){
+    console.log('history')
+    document.getElementById('history-list').classList.remove('hidden')
+    document.getElementById('donation-section').classList.add('hidden')
+    
 })
 
+//---------Donation section------------
+document.getElementById('donation-btn').addEventListener('click', function(){
+    console.log('donation')
+    document.getElementById('history-list').classList.add('hidden')
+    document.getElementById('donation-section').classList.remove('hidden')
 
-
-
+})
 
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,4 @@
-
-  
-function getInputFieldValueById (id){
+ function getInputFieldValueById (id){
     const inputDonation = parseFloat(document.getElementById(id).value);
     return inputDonation ;   
 }
@@ -8,4 +6,16 @@ function getInputFieldValueById (id){
 function getTextFieldValueById(id){
     const totalAmount = parseFloat(document.getElementById(id).innerText);
     return totalAmount;
+}
+
+//--Create addHistory function for history section-----
+function addHistory(cause,amount){
+    const historyCard = document.createElement('div');
+    historyCard.className = "bg-white rounded-md mb-4 p-3 border-2 border-gray-500 ";
+
+    historyCard.innerHTML = `
+        <p><strong>${amount} BDT</strong> is Donated for ${cause}</p>
+        <p>Date: ${new Date().toLocaleString('en-GB', { timeZone: 'Europe/Helsinki' })}</p>
+    `;
+    document.getElementById('history-list').appendChild(historyCard);
 }
