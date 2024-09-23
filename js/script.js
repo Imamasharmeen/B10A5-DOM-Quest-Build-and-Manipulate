eb80589f7fb47619c7cd1fab383ffe3ab5a85f3c
Imamasharmeen <imamasharmeen@gmail.com>
2024-09-23 19:36:43 +0300

    eventListener added in feni section

--- js/script.js
+++ js/script.js
@@ -15,8 +15,54 @@ document.getElementById('noakhali-donation').addEventListener('click', function(
 
     const newRemainingBalance = remainingBalance - noakhaliDonation;
     document.getElementById('remaining-balance').innerText = newRemainingBalance; 
+
+    document.getElementById('my_modal_4').showModel();
+
 })
 
+
+document.getElementById('feni-donation').addEventListener('click', function(){
+    const feniDonation = getInputFieldValueById('feni-donation-amount')
+    const remainingBalance = getTextFieldValueById('remaining-balance')
+    
+
+    if(feniDonation < 0 || isNaN(feniDonation)){
+        alert('Invalid Donation Amount')                           
+    }
+    else{
+        const feniTotalAmount = getTextFieldValueById('feni-total-amount')
+        const newFeniBalance = feniTotalAmount + feniDonation;
+        document.getElementById('feni-total-amount').innerText = newFeniBalance ;
+        
+    }
+
+    const newRemainingBalance = remainingBalance - feniDonation;
+    document.getElementById('remaining-balance').innerText = newRemainingBalance;
+    
+    
+})
+
+
+document.getElementById('feni-donation').addEventListener('click', function(){
+    const feniDonation = getInputFieldValueById('feni-donation-amount')
+    const remainingBalance = getTextFieldValueById('remaining-balance')
+    
+
+    if(feniDonation < 0 || isNaN(feniDonation)){
+        alert('Invalid Donation Amount')                           
+    }
+    else{
+        const feniTotalAmount = getTextFieldValueById('feni-total-amount')
+        const newFeniBalance = feniTotalAmount + feniDonation;
+        document.getElementById('feni-total-amount').innerText = newFeniBalance ;
+        
+    }
+
+    const newRemainingBalance = remainingBalance - feniDonation;
+    document.getElementById('remaining-balance').innerText = newRemainingBalance;
+    
+    
+})
 
 
 
